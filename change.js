15f9e18a9c0d70a8a887510b4dbaf62d1d1494e4
m4ul <lucas.hanson@ynov.com>
2023-03-24 14:21:42 +0100

    change

--- change.js
+++ change.js
@@ -1,3 +1,7 @@
 const get = (arg) => sourceObject.arg;
 
-const set = (arg, arg2) => sourceObject.arg = arg2;
+
+function set (arg, arg2) {
+    sourceObject.arg = arg2;
+    return sourceObject.arg;
+}
