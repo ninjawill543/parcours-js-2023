f3739cbcdc1e43772d1d98e33327d6f779417f60
m4ul <lucas.hanson@ynov.com>
2023-03-24 10:07:46 +0100

    declarations

---
+++ declarations.js
@@ -0,0 +1,16 @@
+const escapeStr = "`/\"'";
+const arr = [4, '2'];
+const obj = {
+    str : "hello there",
+    num : 3,
+    bool : true,
+    undef: undefined,
+}
+const nested = {
+    arr : [4, undefined, '2'],
+    obj : {
+        str : "hello there",
+        num : 3,
+        bool: false,
+    }
+}
