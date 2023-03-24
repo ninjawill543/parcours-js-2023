683f06532c431830b78e909343c90b79ba9328d6
m4ul <lucas.hanson@ynov.com>
2023-03-24 15:21:33 +0100

    dog-years

--- dog-years.js
+++ dog-years.js
@@ -1,20 +1,20 @@
 function dogYears(name, age){
     switch(name){
         case 'earth':
-            return Number.parseFloat((age * 7 * 1)/31557600).toFixed(2);
+            return Number.parseFloat(((age * 7 * 1)/31557600).toFixed(2));
         case 'mercury':
-            return Number.parseFloat((age * 7 * 0.2408467)/31557600).toFixed(2);
+            return Number.parseFloat(((age * 7 * 0.2408467)/31557600).toFixed(2));
         case 'venus':
-            return Number.parseFloat((age * 7 * 0.61519726)/31557600).toFixed(2);
+            return Number.parseFloat(((age * 7 * 0.61519726)/31557600).toFixed(2));
         case 'mars':
-            return Number.parseFloat((age * 7 * 1.8808158)/31557600).toFixed(2);
+            return Number.parseFloat(((age * 7 * 1.8808158)/31557600).toFixed(2));
         case 'jupiter':
-            return Number.parseFloat((age * 7 * 11.862615)/31557600).toFixed(2);
+            return Number.parseFloat(((age * 7 * 11.862615)/31557600).toFixed(2));
         case 'saturn':
-            return Number.parseFloat((age * 7 * 29.447498)/31557600).toFixed(2);
+            return Number.parseFloat(((age * 7 * 29.447498)/31557600).toFixed(2));
         case 'uranus':
-            return Number.parseFloat((age * 7 * 84.016846)/31557600).toFixed(2);
+            return Number.parseFloat(((age * 7 * 84.016846)/31557600).toFixed(2));
         case 'neptune':
-            return Number.parseFloat((age * 7 * 164.79132)/31557600).toFixed(2);
+            return Number.parseFloat(((age * 7 * 164.79132)/31557600).toFixed(2));
     }
 }
