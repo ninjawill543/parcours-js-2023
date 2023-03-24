dfeff88f9ee6a80247d7ee7a2a771b2a62b12411
m4ul <lucas.hanson@ynov.com>
2023-03-24 11:35:04 +0100

    moreless

---
+++ more-or-less.js
@@ -0,0 +1,15 @@
+function more(arg){
+    return arg + 1;
+}
+
+function less(arg){
+    return arg - 1;
+}
+
+function add(arg, arg2){
+    return arg + arg2;
+}
+
+function sub(arg, arg2){
+    return arg - arg2;
+}
