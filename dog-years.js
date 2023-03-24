5112cfc6a0d36ff11cb5731ebf1da5910709a90a
m4ul <lucas.hanson@ynov.com>
2023-03-24 15:17:21 +0100

    dog-years

--- dog-years.js
+++ dog-years.js
@@ -1,21 +1,20 @@
 function dogYears(name, age){
-    let newage = age * 7;
     switch(name){
         case 'earth':
-            return newage * 1 / 31,557,600;
+            return (age * 7 * 1)/31557600;
         case 'mercury':
-            return newage * 0.2408467 / 31,557,600;
+            return (age * 7 * 0.2408467)/31557600;
         case 'venus':
-            return newage * 0.61519726 / 31,557,600;
+            return (age * 7 * 0.61519726)/31557600;
         case 'mars':
-            return newage * 1.8808158 / 31,557,600;
+            return (age * 7 * 1.8808158)/31557600;
         case 'jupiter':
-            return newage * 11.862615 / 31,557,600;
+            return (age * 7 * 11.862615)/31557600;
         case 'saturn':
-            return newage * 29.447498 / 31,557,600;
+            return (age * 7 * 29.447498)/31557600;
         case 'uranus':
-            return newage * 84.016846 / 31,557,600;
+            return (age * 7 * 84.016846)/31557600;
         case 'neptune':
-            return newage * 164.79132 / 31,557,600;
+            return (age * 7 * 164.79132)/31557600;
     }
 }
