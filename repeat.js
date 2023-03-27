d6b716e2aa7ddc38750ab260d448ea39eeaaa968
m4ul <lucas.hanson@ynov.com>
2023-03-27 12:19:11 +0200

    repeat

--- repeat.js
+++ repeat.js
@@ -1,3 +1,7 @@
 function repeat(string, num){
-    console.log(Array((num + 1)).join(string)); 
+    var arr = [];
+    for (let i = 0; i < num; i++) {
+        arr.push(string);
+    }
+    return arr;
 }
