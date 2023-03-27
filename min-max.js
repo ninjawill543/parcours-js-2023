5a5b155d03487f14eba27db92b24f976da695940
m4ul <lucas.hanson@ynov.com>
2023-03-27 10:16:58 +0200

    minmax

---
+++ min-max.js
@@ -0,0 +1,15 @@
+function max(arg, arg2){
+    if (arg > arg2){
+        return arg;
+    }else{
+        return arg2;
+    }
+}
+
+function min(arg, arg2){
+    if (arg < arg2){
+        return arg;
+    }else{
+        return arg2;
+    }
+}
