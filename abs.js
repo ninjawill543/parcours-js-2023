649e551de6b1a25c55977c1a60c603a0573faeef
m4ul <lucas.hanson@ynov.com>
2023-03-27 10:11:44 +0200

    abs

---
+++ abs.js
@@ -0,0 +1,15 @@
+function isPositive(arg){
+    if (arg > 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function abs(arg){
+    if (isPositive(arg) === true){
+        return arg;
+    }else{
+        return arg * -1;
+    }
+}
