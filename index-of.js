17e0eeea3b0a4235eea77ec26d56537486ae7235
m4ul <lucas.hanson@ynov.com>
2023-03-27 15:29:14 +0200

    index-of

--- index-of.js
+++ index-of.js
@@ -22,18 +22,18 @@ function lastIndexOf(array, word, index){
     if (index != null){
         for (let i = (array.length - index); i >= 0; i--) {
             if (array[i] === word){
-                return i;
+                console.log(i) ;
 
             }
         }
-        return -1;
+        console.log(-1);
     }else{
         for (let i = array.length; i >= 0; i--) {
             if (array[i] === word){
-                return i;
+                console.log(i) ;
 
             }
         }
-        return -1;
+        console.log(-1) ;
     }
 }
