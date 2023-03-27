6fdec624d18f4f0fa90699d8cae006d7e20f6537
m4ul <lucas.hanson@ynov.com>
2023-03-27 15:22:58 +0200

    index-of

--- index-of.js
+++ index-of.js
@@ -1,6 +1,12 @@
 function indexOf(array, word, index){
     if (index != null){
+        for (let i = index; i < array.length; i++) {
+            if (array[i] === word){
+                return i;
 
+            }
+        }
+        return -1;
     }else{
         for (let i = 0; i < array.length; i++) {
             if (array[i] === word){
