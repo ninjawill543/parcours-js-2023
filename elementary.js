879662a36e8693aadba695ae976f52f8a0b8ccb7
m4ul <lucas.hanson@ynov.com>
2023-03-27 12:27:54 +0200

    repeat

--- elementary.js
+++ elementary.js
@@ -1,7 +1,15 @@
 function multiply(a, b){
     var sum = 0;
-    for (let i = 0; i < b; i++) {
-        sum = sum + a;
+    if (b > 0){
+        for (let i = 0; i < b; i++) {
+            sum = sum + a;
+        }
+        return(sum);
+    }else{
+        for (let i = 0; i < b; i--) {
+            sum = sum + a;
+        }
+        return(sum);
     }
-    return(sum);
+    
 }
