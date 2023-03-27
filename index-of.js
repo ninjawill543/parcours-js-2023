20a21205381c3837fa138d316733b2a480189478
m4ul <lucas.hanson@ynov.com>
2023-03-27 15:24:27 +0200

    index-of

--- index-of.js
+++ index-of.js
@@ -17,3 +17,23 @@ function indexOf(array, word, index){
         return -1;
     }
 }
+
+function lastIndexOf(array, word, index){
+    if (index != null){
+        for (let i = array.length; i >= index; i--) {
+            if (array[i] === word){
+                return i;
+
+            }
+        }
+        return -1;
+    }else{
+        for (let i = array.length; i >= 0; i--) {
+            if (array[i] === word){
+                return i;
+
+            }
+        }
+        return -1;
+    }
+}
