d67927d5eb5e0e7ea83692198173d642afa2360b
m4ul <lucas.hanson@ynov.com>
2023-03-27 14:17:28 +0200

    elementary

--- elementary.js
+++ elementary.js
@@ -13,3 +13,13 @@ function multiply(a, b){
     }
     
 }
+
+function divide(a, b){
+    var rep = a;
+    var count = 0;
+    while (rep > b){
+        rep = rep - b;
+        count ++;
+    }
+    return count;
+}
