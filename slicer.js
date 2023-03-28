95cd1d73b13cd4a63a27d7bd2b8dee59f5f9da2d
m4ul <lucas.hanson@ynov.com>
2023-03-28 11:02:55 +0200

    slicer

--- slicer.js
+++ slicer.js
@@ -2,12 +2,12 @@ function slice(array, start, end){
     var newarray = [];
     if (end === null){
         for (let i = start; i < array.length; i++) {
-             newarray = newarray + array[i];
+            newarray.push(array[i]);
         }
         return newarray;
     }else{
         for (let i = start; i < end; i++) {
-            newarray = newarray + array[i];
+            newarray.push(array[i]);
        }
        return newarray;
     }
