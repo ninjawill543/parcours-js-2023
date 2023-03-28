6ccf122881c190d80ba8ba3eaa1c20b07140d7f0
m4ul <lucas.hanson@ynov.com>
2023-03-28 12:23:05 +0200

    reverser

---
+++ reverser.js
@@ -0,0 +1,15 @@
+function reverse(input){
+    let sliced = Array.isArray(input) ? [] : '';
+    if (Array.isArray(input)){
+        for (let i = input.length - 1; i >= 0; i--) {
+            sliced.push(input[i]);
+        }
+    }else{
+        for (let i = input.length - 1; i >= 0; i--) {
+            sliced += arr[i];
+        }
+    }
+    return sliced;
+    
+}
+
