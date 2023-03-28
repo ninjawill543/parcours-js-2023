a3315c05051cdf1f8b9092885f49ba94478c28e9
m4ul <lucas.hanson@ynov.com>
2023-03-28 15:42:47 +0200

    nasa

---
+++ nasa.js
@@ -0,0 +1,27 @@
+const nasaCountdown = (count) => {
+    let countdownArray = [];
+
+    for (let i = 1; i <= count; i++) {
+        countdownArray.push(i);
+    }
+
+    for (let j = 0; j < countdownArray.length; j++) {
+        if (countdownArray[j] % 3 === 0 && countdownArray[j] % 5 === 0) {
+            countdownArray[j] = "NASA";
+        } else if (countdownArray[j] % 3 === 0) {
+            countdownArray[j] = "NA";
+        } else if (countdownArray[j] % 5 === 0) {
+            countdownArray[j] = "SA";
+        }
+    }
+
+    let resultString = '';
+    for (let k = 0; k < countdownArray.length; k++) {
+        resultString += countdownArray[k];
+        if (k !== countdownArray.length - 1) {
+            resultString += ' ';
+        }
+    }
+
+    return resultString;
+}
