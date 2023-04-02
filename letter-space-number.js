82c51cb6080245ea751af075ba0c648de800ac6c
m4ul <lucas.hanson@ynov.com>
2023-04-02 11:50:51 +0200

    letterspacenum

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,6 +1,8 @@
-function letterSpaceNumber(str) {
-    const pattern = /\b[a-zA-Z]\s\d(?!\d|[a-zA-Z])/g;
-    const matches = str.match(pattern);
-    return matches ? matches : [];
-  }
-  
+const letterSpaceNumber = (str) => {
+    const regex = /[a-zA-Z]\s\d(?![0-9a-zA-Z])/g
+
+    if (str.match(new RegExp(regex)) == null) {
+        return []
+    }
+    return str.match(new RegExp(regex))
+}
